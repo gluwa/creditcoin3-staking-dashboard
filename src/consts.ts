48aa88bac95268b982d9d3118364aebb51e12d0f
Ada Anderson <ada.anderson@gluwa.com>
2023-08-31 09:40:06 -0400

    Update default network to mainnet

--- src/consts.ts
+++ src/consts.ts
@@ -11,7 +11,7 @@ import type { Plugin } from 'types';
 export const AppVersion = '1.0.8';
 export const DappName = 'Creditcoin Staking Dashboard';
 export const CreditcoinUrl = 'https://creditcoin.org';
-export const DefaultNetwork = 'creditcoinTest';
+export const DefaultNetwork = 'creditcoin';
 export const ManualSigners = ['ledger', 'vault'];
 /*
  * Data Structure Helpers
