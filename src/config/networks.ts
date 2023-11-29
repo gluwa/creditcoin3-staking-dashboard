4ed20995176e1a3e53e85c1f19f5b92b8098c664
Nathan Whitaker <nathan.whitaker01@gmail.com>
2023-11-28 21:45:16 -0500

    clean up networks a bit

--- src/config/networks.ts
+++ src/config/networks.ts
@@ -5,13 +5,80 @@ import { DefaultParams } from 'consts';
 import CreditcoinIconSVG from 'img/ic_creditcoin.svg?react';
 import CreditcoinLogoSVG from 'img/logo_creditcoin.svg?react';
 
-import type { Networks } from 'types';
+import type { Network, NetworkName, Networks } from 'types';
 
 const CreditcoinTokenSVG = CreditcoinIconSVG;
 
+type NetworkConfig = {
+  name: NetworkName;
+  endpoints: Network['endpoints'];
+  namespace?: string;
+  subscanEndpoint: string;
+};
+
+const makeCreditcoinNetwork = ({
+  name,
+  endpoints,
+  namespace,
+  subscanEndpoint,
+}: NetworkConfig): Network => {
+  return {
+    name,
+    endpoints,
+    namespace: namespace ?? '09573a3526818a8ecd6eb92f60f1175d',
+    subscanEndpoint,
+
+    api: {
+      unit: 'CTC',
+      priceTicker: 'CTCUSDT',
+    },
+    params: {
+      ...DefaultParams,
+    },
+    ss58: 42,
+    unit: 'CTC',
+    units: 18,
+    brand: {
+      icon: CreditcoinIconSVG,
+      token: CreditcoinTokenSVG,
+      logo: {
+        svg: CreditcoinLogoSVG,
+        width: '7.2em',
+      },
+      inline: {
+        svg: CreditcoinIconSVG,
+        size: '1.05em',
+      },
+    },
+    colors: {
+      primary: {
+        light: 'rgb(211, 48, 121)',
+        dark: 'rgb(211, 48, 121)',
+      },
+      secondary: {
+        light: '#552bbf',
+        dark: '#6d39ee',
+      },
+      stroke: {
+        light: 'rgb(211, 48, 121)',
+        dark: 'rgb(211, 48, 121)',
+      },
+      transparent: {
+        light: 'rgb(211, 48, 121, 0.05)',
+        dark: 'rgb(211, 48, 121, 0.05)',
+      },
+      pending: {
+        light: 'rgb(211, 48, 121, 0.33)',
+        dark: 'rgb(211, 48, 121, 0.33)',
+      },
+    },
+    defaultFeeReserve: 0.1,
+  };
+};
+
 const makeNetworkList = () => {
   const networks: Networks = {
-    creditcoin: {
+    creditcoin: makeCreditcoinNetwork({
       name: 'creditcoin',
       endpoints: {
         lightClient: null,
@@ -21,54 +88,9 @@ const makeNetworkList = () => {
         },
       },
       namespace: 'creditcoin-mainnet',
-      api: {
-        unit: 'CTC',
-        priceTicker: 'CTCUSDT',
-      },
-      params: {
-        ...DefaultParams,
-      },
-      ss58: 42,
-      unit: 'CTC',
-      units: 18,
-      brand: {
-        icon: CreditcoinIconSVG,
-        token: CreditcoinTokenSVG,
-        logo: {
-          svg: CreditcoinLogoSVG,
-          width: '7.2em',
-        },
-        inline: {
-          svg: CreditcoinIconSVG,
-          size: '1.05em',
-        },
-      },
-      colors: {
-        primary: {
-          light: 'rgb(211, 48, 121)',
-          dark: 'rgb(211, 48, 121)',
-        },
-        secondary: {
-          light: '#552bbf',
-          dark: '#6d39ee',
-        },
-        stroke: {
-          light: 'rgb(211, 48, 121)',
-          dark: 'rgb(211, 48, 121)',
-        },
-        transparent: {
-          light: 'rgb(211, 48, 121, 0.05)',
-          dark: 'rgb(211, 48, 121, 0.05)',
-        },
-        pending: {
-          light: 'rgb(211, 48, 121, 0.33)',
-          dark: 'rgb(211, 48, 121, 0.33)',
-        },
-      },
       subscanEndpoint: 'http://127.0.0.1:4399',
-      defaultFeeReserve: 0.1,
-    },
-    creditcoinTest: {
+    }),
+    creditcoinTest: makeCreditcoinNetwork({
       name: 'creditcoinTest',
       endpoints: {
         lightClient: null,
@@ -78,54 +100,9 @@ const makeNetworkList = () => {
         },
       },
       namespace: '09573a3526818a8ecd6eb92f60f1175d',
-      api: {
-        unit: 'CTC',
-        priceTicker: 'CTCUSDT',
-      },
-      params: {
-        ...DefaultParams,
-      },
-      ss58: 42,
-      unit: 'CTC',
-      units: 18,
-      brand: {
-        icon: CreditcoinIconSVG,
-        token: CreditcoinTokenSVG,
-        logo: {
-          svg: CreditcoinLogoSVG,
-          width: '7.2em',
-        },
-        inline: {
-          svg: CreditcoinIconSVG,
-          size: '1.05em',
-        },
-      },
-      colors: {
-        primary: {
-          light: 'rgb(211, 48, 121)',
-          dark: 'rgb(211, 48, 121)',
-        },
-        secondary: {
-          light: '#552bbf',
-          dark: '#6d39ee',
-        },
-        stroke: {
-          light: 'rgb(211, 48, 121)',
-          dark: 'rgb(211, 48, 121)',
-        },
-        transparent: {
-          light: 'rgb(211, 48, 121, 0.05)',
-          dark: 'rgb(211, 48, 121, 0.05)',
-        },
-        pending: {
-          light: 'rgb(211, 48, 121, 0.33)',
-          dark: 'rgb(211, 48, 121, 0.33)',
-        },
-      },
       subscanEndpoint: 'https://subscan-testnet.creditcoin.network/',
-      defaultFeeReserve: 0.1,
-    },
-    creditcoinDev: {
+    }),
+    creditcoinDev: makeCreditcoinNetwork({
       name: 'creditcoinDev',
       endpoints: {
         lightClient: null,
@@ -135,56 +112,11 @@ const makeNetworkList = () => {
         },
       },
       namespace: '09573a3526818a8ecd6eb92f60f1175d',
-      api: {
-        unit: 'CTC',
-        priceTicker: 'CTCUSDT',
-      },
-      params: {
-        ...DefaultParams,
-      },
-      ss58: 42,
-      unit: 'CTC',
-      units: 18,
-      brand: {
-        icon: CreditcoinIconSVG,
-        token: CreditcoinTokenSVG,
-        logo: {
-          svg: CreditcoinLogoSVG,
-          width: '7.2em',
-        },
-        inline: {
-          svg: CreditcoinIconSVG,
-          size: '1.05em',
-        },
-      },
-      colors: {
-        primary: {
-          light: 'rgb(211, 48, 121)',
-          dark: 'rgb(211, 48, 121)',
-        },
-        secondary: {
-          light: '#552bbf',
-          dark: '#6d39ee',
-        },
-        stroke: {
-          light: 'rgb(211, 48, 121)',
-          dark: 'rgb(211, 48, 121)',
-        },
-        transparent: {
-          light: 'rgb(211, 48, 121, 0.05)',
-          dark: 'rgb(211, 48, 121, 0.05)',
-        },
-        pending: {
-          light: 'rgb(211, 48, 121, 0.33)',
-          dark: 'rgb(211, 48, 121, 0.33)',
-        },
-      },
       subscanEndpoint: 'http://127.0.0.1:4399',
-      defaultFeeReserve: 0.1,
-    },
+    }),
   };
   if (import.meta.env.DEV) {
-    networks.creditcoinLocal = {
+    networks.creditcoinLocal = makeCreditcoinNetwork({
       name: 'creditcoinLocal',
       endpoints: {
         lightClient: null,
@@ -193,54 +125,8 @@ const makeNetworkList = () => {
           Local: 'ws://127.0.0.1:9944',
         },
       },
-      namespace: '09573a3526818a8ecd6eb92f60f1175d',
-      api: {
-        unit: 'CTC',
-        priceTicker: 'CTCUSDT',
-      },
-      params: {
-        ...DefaultParams,
-      },
-      ss58: 42,
-      unit: 'CTC',
-      units: 18,
-      brand: {
-        icon: CreditcoinIconSVG,
-        token: CreditcoinTokenSVG,
-        logo: {
-          svg: CreditcoinLogoSVG,
-          width: '7.2em',
-        },
-        inline: {
-          svg: CreditcoinIconSVG,
-          size: '1.05em',
-        },
-      },
-      colors: {
-        primary: {
-          light: 'rgb(211, 48, 121)',
-          dark: 'rgb(211, 48, 121)',
-        },
-        secondary: {
-          light: '#552bbf',
-          dark: '#6d39ee',
-        },
-        stroke: {
-          light: 'rgb(211, 48, 121)',
-          dark: 'rgb(211, 48, 121)',
-        },
-        transparent: {
-          light: 'rgb(211, 48, 121, 0.05)',
-          dark: 'rgb(211, 48, 121, 0.05)',
-        },
-        pending: {
-          light: 'rgb(211, 48, 121, 0.33)',
-          dark: 'rgb(211, 48, 121, 0.33)',
-        },
-      },
       subscanEndpoint: 'http://127.0.0.1:4399',
-      defaultFeeReserve: 0.1,
-    };
+    });
   }
   return networks;
 };
