gluwa/creditcoin-staking-dashboard

Fix incosistent filter behavior in validator list; Fix duplicates when autogenerating validator list

src/library/GenerateNominations/useFetchMethods.tsx
@@ -112,7 +112,9 @@ export const useFetchMehods = () => {
       _nominationsActive = shuffle(_nominationsActive).slice(0, 12);
     }
 
-    return shuffle(_nominationsWaiting.concat(_nominationsActive));
+    return shuffle([
+      ...new Set(_nominationsWaiting.concat(_nominationsActive)),
+    ]);
   };
 
   const available = (nominations: any) => {

gluwa/creditcoin3-staking-dashboard

src/library/GenerateNominations/useFetchMethods.tsx
@@ -120,7 +120,9 @@ export const useFetchMehods = () => {
       active = shuffle(active).slice(0, 14);
     }
 
-    return shuffle(waiting.concat(active));
+    return shuffle([
+      ...new Set(waiting.concat(active)),
+    ]);
   };
 
   const available = (nominations: Validator[]) => {