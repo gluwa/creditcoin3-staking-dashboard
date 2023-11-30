gluwa/creditcoin-staking-dashboard

[SS-174] Guage color changed

src/library/BarChart/Wrappers.ts
@@ -27,6 +27,9 @@ export const BarChartWrapper = styled.div<{ lessPadding?: boolean }>`
   .d1 {
     background: var(--network-color-primary);
     color: rgba(255, 255, 255, 0.9);
+    span {
+      color: var(--text-color-secondary);
+    }
   }
   .d2 {
     background: var(--network-color-secondary);

gluwa/creditcoin3-staking-dashboard

src/library/BarChart/Wrappers.ts
@@ -28,6 +28,9 @@ export const BarChartWrapper = styled.div<{ $lessPadding?: boolean }>`
   .d1 {
     background: var(--accent-color-primary);
     color: rgba(255, 255, 255, 0.95);
+    span {
+      color: var(--accent-color-secondary);
+    }
   }
   .d2 {
     background: var(--accent-color-secondary);