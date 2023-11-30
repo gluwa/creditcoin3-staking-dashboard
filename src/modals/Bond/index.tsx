nathanwhit/creditcoin-staking-dashboard

Convert big numbers to string with `toFixed`
instead of `toString` which produces scientific notation
for large numbers, as polkadotJS cannot handle it

src/modals/Bond/index.tsx
@@ -48,7 +48,7 @@ export const Bond = () => {
 
   // local bond value.
   const [bond, setBond] = useState<{ bond: string }>({
-    bond: freeBalance.toString(),
+    bond: freeBalance.toFixed(),
   });
 
   // bond valid.
@@ -73,8 +73,8 @@ export const Bond = () => {
 
   // update bond value on task change.
   useEffect(() => {
-    setBond({ bond: freeBalance.toString() });
-  }, [freeBalance.toString()]);
+    setBond({ bond: freeBalance.toFixed() });
+  }, [freeBalance.toFixed()]);
 
   // modal resize on form update
   useEffect(() => {
@@ -92,7 +92,7 @@ export const Bond = () => {
       ? '0'
       : bondToSubmit.isNaN()
       ? '0'
-      : bondToSubmit.toString();
+      : bondToSubmit.toFixed();
 
     if (isPooling) {
       tx = api.tx.nominationPools.bondExtra({

gluwa/creditcoin3-staking-dashboard

src/modals/Bond/index.tsx
@@ -60,7 +60,7 @@ export const Bond = () => {
 
   // local bond value.
   const [bond, setBond] = useState<{ bond: string }>({
-    bond: freeToBond.toString(),
+    bond: freeToBond.toFixed(),
   });
 
   // bond valid.
@@ -88,8 +88,8 @@ export const Bond = () => {
 
   // update bond value on task change.
   useEffect(() => {
-    setBond({ bond: freeToBond.toString() });
-  }, [freeToBond.toString()]);
+    setBond({ bond: freeToBond.toFixed() });
+  }, [freeToBond.toFixed()]);
 
   // determine whether this is a pool or staking transaction.
   const determineTx = (bondToSubmit: BigNumber) => {
@@ -102,7 +102,7 @@ export const Bond = () => {
       ? '0'
       : bondToSubmit.isNaN()
       ? '0'
-      : bondToSubmit.toString();
+      : bondToSubmit.toFixed();
 
     if (isPooling) {
       tx = api.tx.nominationPools.bondExtra({