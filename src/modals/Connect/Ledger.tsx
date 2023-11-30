nathanwhit/creditcoin-staking-dashboard

Check network against "creditcoin" not polkadot

src/modals/Connect/Ledger.tsx
@@ -39,10 +39,12 @@ export const Ledger = (): React.ReactElement => {
           </div>
           <div className="row margin">
             <ButtonText
-              text={name === 'polkadot' ? 'BETA' : 'EXPERIMENTAL'}
+              text={name === 'creditcoin' ? 'BETA' : 'EXPERIMENTAL'}
               disabled
               marginRight
-              iconLeft={name === 'polkadot' ? undefined : faExclamationTriangle}
+              iconLeft={
+                name === 'creditcoin' ? undefined : faExclamationTriangle
+              }
               style={{ opacity: 0.5 }}
             />
             <ButtonText

gluwa/creditcoin3-staking-dashboard

src/modals/Connect/Ledger.tsx
@@ -44,11 +44,11 @@ export const Ledger = (): React.ReactElement => {
           </div>
           <div className="row margin">
             <ButtonText
-              text={network === 'polkadot' ? 'BETA' : 'EXPERIMENTAL'}
+              text={network === 'creditcoin' ? 'BETA' : 'EXPERIMENTAL'}
               disabled
               marginRight
               iconLeft={
-                network === 'polkadot' ? undefined : faExclamationTriangle
+                network === 'creditcoin' ? undefined : faExclamationTriangle
               }
               style={{ opacity: 0.5 }}
             />