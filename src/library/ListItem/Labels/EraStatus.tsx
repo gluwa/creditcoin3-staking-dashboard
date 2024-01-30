4ec3cd88707571203b9011c05ba9fd3da3ce951c
frankli-dev <51565705+frankli-dev@users.noreply.github.com>
2024-01-30 09:55:20 -0700

    fix: Waiting label for inactive (#77)
    Co-authored-by: Frank Li <frank.li@gluwa.com>

--- src/library/ListItem/Labels/EraStatus.tsx
+++ src/library/ListItem/Labels/EraStatus.tsx
@@ -8,6 +8,7 @@ import { useUi } from 'contexts/UI';
 import { ValidatorStatusWrapper } from 'library/ListItem/Wrappers';
 import { useNetwork } from 'contexts/Network';
 import type { EraStatusProps } from '../types';
+import BigNumber from 'bignumber.js';
 
 export const EraStatus = ({ noMargin, status, totalStake }: EraStatusProps) => {
   const { t } = useTranslation('library');
@@ -23,9 +24,11 @@ export const EraStatus = ({ noMargin, status, totalStake }: EraStatusProps) => {
       <h5>
         {isSyncing || erasStakersSyncing
           ? t('syncing')
-          : `${t('listItemActive')} / ${planckToUnit(totalStake, units)
-              .integerValue()
-              .toFormat()} ${unit}`}
+          : status === 'waiting' && totalStake.comparedTo(BigNumber(0)) === 0
+            ? t('waiting')
+            : `${t('listItemActive')} / ${planckToUnit(totalStake, units)
+                .integerValue()
+                .toFormat()} ${unit}`}
       </h5>
     </ValidatorStatusWrapper>
   );
