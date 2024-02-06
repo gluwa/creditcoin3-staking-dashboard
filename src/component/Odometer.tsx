8dbbce4c568fb53dd8d79f1c6574d7661a1fce7c
nbass3 <70085020+nbass3@users.noreply.github.com>
2024-02-06 22:19:57 +0900

    Fix some code

--- src/component/Odometer.tsx
+++ src/component/Odometer.tsx
@@ -1,32 +1,50 @@
 import { useMemo } from 'react';
 
 interface OdometerProps {
-  value: string;
+  value?: string;
+  spaceBefore?: string;
   spaceAfter?: string;
   zeroDecimals?: number;
+  wholeColor?: string;
+  decimalColor?: string;
 }
 
-export const Odometer = (props: OdometerProps) => {
-  const { value, spaceAfter, zeroDecimals } = props;
+export const Odometer = ({
+  value = '0',
+  spaceBefore = '0px',
+  spaceAfter = '0px',
+  zeroDecimals = 0,
+  wholeColor = 'var(--text-color-primary)',
+  decimalColor = 'var(--text-color-tertiary)',
+}: OdometerProps) => {
   const formattedValue = useMemo(() => {
-    if (value === '0') {
-      return '0';
+    let [whole, decimal] = value.split('.');
+    whole = whole.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    if (decimal) {
+      decimal = parseFloat(`0.${decimal}`).toFixed(zeroDecimals).substring(2);
     }
-    return formatNumber(value, zeroDecimals);
+    return { whole, decimal };
   }, [value, zeroDecimals]);
 
   return (
-    <span>
-      {formattedValue}
-      {spaceAfter && <>&nbsp;{spaceAfter}</>}
+    <span
+      style={{
+        paddingLeft: spaceBefore,
+        paddingRight: spaceAfter,
+        color: wholeColor,
+      }}
+    >
+      {formattedValue.whole.split('').map((digit, index) => (
+        <span key={`whole-${index}`}>{digit}</span>
+      ))}
+      {formattedValue.decimal && (
+        <span style={{ color: decimalColor || wholeColor }}>
+          .
+          {formattedValue.decimal.split('').map((digit, index) => (
+            <span key={`decimal-${index}`}>{digit}</span>
+          ))}
+        </span>
+      )}
     </span>
   );
-};
-
-const formatNumber = (value: string, zeroDecimals?: number) => {
-  const number = parseFloat(value);
-  if (zeroDecimals !== undefined) {
-    return number.toFixed(zeroDecimals);
-  }
-  return number.toLocaleString();
 };
