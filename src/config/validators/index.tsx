bfd9f709f28d9e2c39ce2fafe4fc2549118c3013
Frank Li <frank.li@gluwa.com>
2024-02-09 09:06:30 -0700

    fix: Validator community

---
+++ src/config/validators/index.tsx
@@ -0,0 +1,20 @@
+export const ValidatorCommunity = [
+  {
+    name: 'Gluwa Creditcoin Validators',
+    thumbnail: 'Creditcoin',
+    bio: "Gluwa's Validators",
+    email: 'support@gluwa.com',
+    twitter: '@gluwa',
+    website: 'https://www.gluwa.com/',
+    validators: {
+      creditcoinTest: [
+        '5DcvAgRBWuQfQCGK2zZE5nVw36WgdYrTZhvxD68hZCPW26bZ',
+        '5HGskSoTDbRAfiGxDVj4K4BDJBGxLjDaNvb7QLpURDNTrDjA',
+        '5HeEMRybEWWWrNc1DSAMXuqwyGwpuzcRVv7BvmfF3XyFqvfq',
+        '5FZLEzcPppgLT1mRDWSYKQE2RWwp66NSuxKZpnVmCgB7UVmn',
+        '5EpmD1q1P3qY38YqkM6UXt56Gfrz8vXTu1cVgG2WSibrxphQ',
+        '5CLvDXfB4vpz7knm3Me6C42aFKzz5H6UDg92aRFCuyABdJpZ',
+      ],
+    },
+  },
+];
