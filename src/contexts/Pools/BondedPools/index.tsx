0dd793fe2564ad6388d2f0269042ce4d11e58edd
Juan Gallicchio <juan.gallicchio@gluwa.com>
2025-07-14 17:17:34 +0200

    Improving initialMaxCommission when set to null in pool. Default to globalMaxCommission to prevent Modify pool comission to work.

--- src/contexts/Pools/BondedPools/index.tsx
+++ src/contexts/Pools/BondedPools/index.tsx
@@ -176,8 +176,10 @@ export const BondedPoolsProvider = ({
     addresses: createAccounts(id),
   });
 
-  const getBondedPool = (poolId: MaybePool) =>
-    bondedPools.find((p) => p.id === poolId) ?? null;
+  const getBondedPool = (poolId: MaybePool) => {
+    if (poolId === null) return null;
+    return bondedPools.find((p) => Number(p.id) === Number(poolId)) ?? null;
+  };
 
   /*
    * poolSearchFilter Iterates through the supplied list and refers to the meta batch of the list to
