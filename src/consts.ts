cbce14cf5f2192370d13d94684b62348c395858a
Juan Gallicchio <juan.gallicchio@gluwa.com>
2025-08-05 20:16:31 +0200

    creditcoindryrun is not upgraded

--- src/consts.ts
+++ src/consts.ts
@@ -13,11 +13,7 @@ export const DappName = 'Creditcoin Staking Dashboard';
 export const CreditcoinUrl = 'https://creditcoin.org';
 export const DefaultNetwork = 'creditcoin';
 export const ManualSigners = ['ledger', 'vault'];
-export const UpgradedNetworks = [
-  'creditcoindryrun',
-  'creditcoinDev',
-  'creditcoinTest',
-];
+export const UpgradedNetworks = ['creditcoinDev', 'creditcoinTest'];
 /*
  * Data Structure Helpers
  */
